eb478a78313d61c3cb14fbe0ebf19df40a8b119b
Edgar <edvmaster.com@gmail.com>
2023-01-15 15:37:47 -0600

    youtube feat for doogos

--- Petfinder-project-resources/Data/doggofact.js
+++ Petfinder-project-resources/Data/doggofact.js
@@ -1,24 +1,46 @@
-async function start() {
-  const response = await fetch( "" )
-  const data = await response.json()
-  CreateFactList(data.message)
-}
+// error codes apis
+https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCCUVL6CLdcHFE4urCHrNDz_9WqrsJ8LW4
+var error = 'https://http.dog/[code].jpg'
+var youtubeSearch = document.querySelector ('#form');
+var API_KEY = "AIzaSyCCUVL6CLdcHFE4urCHrNDz_9WqrsJ8LW4"
 
-start()
+const form = document.getElementById('search-form');
+const input = document.getElementById('search-input');
 
-function CreateFactList(factList) {
-  document.getElementById("Drop").innerHTML = `
-  <select onchange="loadByBreed(this.value)" > 
-      <option> Choose something </option> 
-      
-      ${Object.keys(factList).map(function(fact) { 
-          return `<option>${fact}</option>`
 
-}).join('')}
-    </select>
-  `   
-}
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = input.value;
+  
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${"how to take care of a" + query}&key=AIzaSyCCUVL6CLdcHFE4urCHrNDz_9WqrsJ8LW4&type=video&maxResults=1`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Extract the video titles and thumbnails from the data
+      // and display them on the page
 
-function loadByBreed (fact){
-  alert
-}
+const resultsContainer = document.getElementById('search-results');
+const videos = data.items;
+const videoList = document.createElement('ul');
+
+videos.forEach(video => {
+  const videoTitle = video.snippet.title;
+  const videoThumb = video.snippet.thumbnails.default.url;
+  const videoId = video.id.videoId;
+  
+  const videoItem = document.createElement('li');
+  videoItem.innerHTML = `
+    <img src="${videoThumb}">
+    <a href="https://www.youtube.com/watch?v=${videoId}">${videoTitle}</a>
+  `;
+  
+  videoList.appendChild(videoItem);
+resultsContainer.appendChild(videoList);
+
+    })})
+  .catch(error => console.error(error));
+});
+
+
