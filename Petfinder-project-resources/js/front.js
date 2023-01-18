939ff3fc04344f352eed541696e028550cef725e
Edgar <edvmaster.com@gmail.com>
2023-01-17 21:34:06 -0600

    youtube searches up animal planet channel with 1 video result for the breed

--- Petfinder-project-resources/js/front.js
+++ Petfinder-project-resources/js/front.js
@@ -8,6 +8,8 @@ var animalArr=[[]];
 var animalPhoto = [[]];
 var pf = new petfinder.Client({apiKey: key, secret: secretKey});
 
+
+
 const zipInput = document.getElementById('inputZip');
 const zipBtn = document.getElementById('zipBtn');
 const cards = [[document.getElementById('cardOneName'), document.getElementById('animalOneSpecies/Breed'), 
