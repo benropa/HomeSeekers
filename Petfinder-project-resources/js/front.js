fb299dcf47d1ce6c10fa4a5b25ae9e34f227fc8d
Edgar <edvmaster.com@gmail.com>
2023-01-18 21:57:15 -0600

    merge

--- Petfinder-project-resources/js/front.js
+++ Petfinder-project-resources/js/front.js
@@ -7,6 +7,7 @@ var orgPhoto = [[]];
 var animalArr=[[]];
 var animalPhoto = [[]];
 var pf = new petfinder.Client({apiKey: key, secret: secretKey});
+var boxes = document.getElementById('boxes')
 
 
 
@@ -119,9 +120,44 @@ zipBtn.addEventListener('click', function() {
     }
 });
 
-$('.boxes').on('click', function(event) {
-    event.preventDefault();
-    var url = './petDisplay.html?animalID=' + event.target.parentElement.attributes.value.nodeValue;
-    window.location.href = url;
-});
 
+
+
+$('.boxes').on('click', () => { 
+  
+  const Breed = "dog"
+  console.log(Breed)
+  
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCkEBDbzLyH-LbB2FgMoSMaQ&q=${ Breed + " facts" }&key=AIzaSyCCUVL6CLdcHFE4urCHrNDz_9WqrsJ8LW4&maxResults=1`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+const resultsContainer = document.getElementById('search-results');
+const videos = data.items;
+const videoList = document.createElement('ul');
+
+videos.forEach(video => {
+  const videoId = video.id.videoId;
+  const videoItem = document.createElement('li');
+  const videoDis = document.createElement('li');
+  videoItem.innerHTML = `
+  <iframe width="420" height="315"
+  src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen >
+  </iframe>
+  `;
+  videoDis.innerHTML = `
+    
+    <p> Video link: https://www.youtube.com/channel/watch?v=${videoId} </p>
+  `;
+
+  
+  videoList.appendChild(videoDis);
+  videoList.appendChild(videoItem);
+resultsContainer.appendChild(videoList);
+
+    })})
+
+});
