18cd069e2667a8bb35730372b70aaf2da1309f01
stev232 <stev232@yahoo.com>
2023-01-18 21:48:19 -0600

    Populated the main animal page and displaying youtube video about their breed.

---
+++ Petfinder-project-resources/Data/doggovid.js
@@ -0,0 +1,71 @@
+// error codes apis
+// var error = 'https://http.dog/[code].jpg'
+
+// https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCCUVL6CLdcHFE4urCHrNDz_9WqrsJ8LW4
+
+var animalArr;
+var animalPhoto;
+var orgInfo;
+var orgPhoto;
+var Breed;
+const currentURL = new URL(window.location.toLocaleString()).searchParams;
+const animalID = currentURL.get('animalID');
+
+function load() {
+  animalArr = localStorage.getItem('animalArr').split(',');
+  animalPhoto = localStorage.getItem('animalPhoto').split(',');
+  orgInfo = localStorage.getItem('orgInfo').split(',');
+  orgPhoto = localStorage.getItem('orgPhoto').split(',');
+}
+
+load();
+
+function searchAnimalArr() {
+  for(var i = 0; i < animalArr.length; i+=8) {
+    if(animalID == animalArr[i]) {
+      document.getElementById('cardOneName').setAttribute("style", "background-image: url(" + animalPhoto[8*(i/8)+3*(i/8)-i] + ");");
+      document.getElementById('animalOneName').textContent = animalArr[i+1]; 
+      document.getElementById('animalOneSpeciesBreed').textContent = animalArr[i+5]+ " / " + animalArr[i+4]; 
+      document.getElementById('animalOneGender').textContent = animalArr[i+3]; 
+      document.getElementById('animalOneAge').textContent = animalArr[i+7]; 
+      document.getElementById('animalOneSize').textContent = animalArr[i+6];
+      Breed = animalArr[i+4];
+    }
+  }
+}
+
+searchAnimalArr();
+
+  
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCkEBDbzLyH-LbB2FgMoSMaQ&q=${ Breed + " facts" }&key=AIzaSyCCUVL6CLdcHFE4urCHrNDz_9WqrsJ8LW4&maxResults=1`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+const resultsContainer = document.getElementById('search-results');
+const videos = data.items;
+const videoList = document.createElement('ul');
+
+videos.forEach(video => {
+  const videoId = video.id.videoId;
+  const videoItem = document.createElement('li');
+  const videoDis = document.createElement('li');
+  videoItem.innerHTML = `
+  <iframe width="420" height="315"
+  src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen >
+  </iframe>
+  `;
+  videoDis.innerHTML = `
+    
+    <p> Video link: https://www.youtube.com/channel/watch?v=${videoId} </p>
+  `;
+
+  
+  videoList.appendChild(videoDis);
+  videoList.appendChild(videoItem);
+resultsContainer.appendChild(videoList);
+
+    })
+  })
--- Petfinder-project-resources/js/front.js
+++ Petfinder-project-resources/js/front.js
@@ -42,6 +42,8 @@ function findOrganizations() {
                     orgPhoto[i][2]=resp.data.organizations[i].photos[0].large;
                 }
             }
+            save('orgInfo', orgInfo);
+            save('orgPhoto', orgPhoto);
         });
 }
 
@@ -70,7 +72,8 @@ function findAnimal() {
                     animalPhoto[i][2] = response.data.animals[i].photos[0].large;
                 }
             }
-            save();
+            save('animalArr', animalArr);
+            save('animalPhoto', animalPhoto);
 
             for(var i = 0; i < cards.length; i++) {
                 cards[i][0].setAttribute("value", animalArr[i][0]);
@@ -87,11 +90,8 @@ function findAnimal() {
         }); 
 }
 
-function save() {
-    localStorage.setItem('orgInfo', orgInfo);
-    localStorage.setItem('orgPhoto', orgPhoto);
-    localStorage.setItem('animalArr', animalArr);
-    localStorage.setItem('animalPhoto', animalPhoto);
+function save(name, data) {
+    localStorage.setItem(name, data);
 }
 
 findOrganizations();
